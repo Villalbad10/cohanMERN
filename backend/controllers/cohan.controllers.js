c569a85eaa61c19f7b13f622bfb643b4d31eb0ac
Diego Villalba <diego.villalba01@uptc.edu.co>
2022-09-04 00:04:49 -0500

    control de errores del serv

--- backend/controllers/cohan.controllers.js
+++ backend/controllers/cohan.controllers.js
@@ -1,42 +1,58 @@
 import { poll } from "../db.js";
 
 export const getCohan = async (req, res) => {
-  const [result] = await poll.query("SELECT * FROM person");
-  res.json(result);
+  try {
+    const [result] = await poll.query("SELECT * FROM person");
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
   //console.log(result);
 };
 
 export const createCohan = async (req, res) => {
-  const { nombre, phone, email } = req.body;
-  const [result] = await poll.query(
-    "INSERT INTO person(nombre, phone, email) VALUES (?, ?, ?)",
-    [nombre, phone, email]
-  );
-  res.json({
-    id: result.insertId,
-    nombre,
-    phone,
-    email,
-  });
+  try {
+    const { nombre, phone, email } = req.body;
+    const [result] = await poll.query(
+      "INSERT INTO person(nombre, phone, email) VALUES (?, ?, ?)",
+      [nombre, phone, email]
+    );
+    res.json({
+      id: result.insertId,
+      nombre,
+      phone,
+      email,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const updateCohan = async (req, res) => {
-  const result = await poll.query("UPDATE person SET ? WHERE id = ?", [
-    req.body,
-    req.params.id,
-  ]);
+  try {
+    const result = await poll.query("UPDATE person SET ? WHERE id = ?", [
+      req.body,
+      req.params.id,
+    ]);
 
-  res.json(result);
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const deleteCohan = async (req, res) => {
-  const [result] = await poll.query("DELETE FROM person WHERE id = ?", [
-    req.params.id,
-  ]);
+  try {
+    const [result] = await poll.query("DELETE FROM person WHERE id = ?", [
+      req.params.id,
+    ]);
 
-  if (result.affectedRows === 0) {
-    return res.status(404).json({ message: "Persona no encontrada" });
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ message: "Persona no encontrada" });
+    }
+
+    return res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
-
-  return res.sendStatus(204);
 };
