114adaa2932a3d251bdcf1414e26c471a208b569
Diego Villalba <diego.villalba01@uptc.edu.co>
2022-09-03 21:43:50 -0500

    rutas del servidor

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { PORT } from "./config.js";
-import indexRoutes from './routes/index.routes.js';
+import indexRoutes from "./routes/index.routes.js";
+import cohanRoutes from "./routes/cohan.routes.js";
 
 const app = express();
+app.use(cohanRoutes);
 app.use(indexRoutes);
 
 app.listen(PORT);
--- backend/routes/cohan.routes.js
+++ backend/routes/cohan.routes.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
+import {
+  createCohan,
+  deleteCohan,
+  getCohan,
+  updateCohan,
+} from "../controllers/cohan.controllers.js";
 
 const router = Router();
 
-router.get("/cohan");
-router.post("/cohan");
-router.put("/cohan/:id");
-router.delete("/cohan/:id");
+router.get("/cohan", getCohan);
+router.post("/cohan", createCohan);
+router.put("/cohan/:id", updateCohan);
+router.delete("/cohan/:id", deleteCohan);
 
 export default router;
