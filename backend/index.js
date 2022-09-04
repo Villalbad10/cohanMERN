15e136fe64601867c8c6a434e693fd25059021c1
Diego Villalba <diego.villalba01@uptc.edu.co>
2022-09-03 23:11:55 -0500

    post a DB

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ import indexRoutes from "./routes/index.routes.js";
 import cohanRoutes from "./routes/cohan.routes.js";
 
 const app = express();
+
+app.use(express.json());
+
 app.use(cohanRoutes);
 app.use(indexRoutes);
 
