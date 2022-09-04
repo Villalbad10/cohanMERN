5197af856ad9dfdebf2e6479af0177cb97388b12
Diego Villalba <diego.villalba01@uptc.edu.co>
2022-09-04 11:56:16 -0500

    rutas publicas y privadas

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,12 +1,53 @@
-import React from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { dataLogin } from "../app/user.slice";
 import Login from "../containers/Login";
+import { DasboardRouter } from "./DashboarRouter";
+import PrivateRoutes from "./PrivateRoutes";
+import PublicRoutes from "./PublicRoutes";
 
 const AppRoutes = () => {
+  const dispatch = useDispatch();
+  const [checkAuth, setCheckAuth] = useState(true);
+  const [logget, setLogget] = useState(false);
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user?.uid) {
+        setLogget(true);
+        dispatch(dataLogin(user.reloadUserInfo));
+      } else {
+        setLogget(false);
+      }
+      setCheckAuth(false);
+    });
+  }, [setLogget, setCheckAuth]);
+
+  if (checkAuth) return <h1 style={{ color: "black" }}>Espere...</h1>;
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route
+          path="/login"
+          element={
+            <PublicRoutes isAuth={logget}>
+              <Login />
+            </PublicRoutes>
+          }
+        />
+
+        <Route
+          path="/*"
+          element={
+            <PrivateRoutes isAuth={logget}>
+              <DasboardRouter />
+            </PrivateRoutes>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
