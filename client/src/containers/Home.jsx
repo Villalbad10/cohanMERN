c83a98d48d9da07701e7e93ccebb54f0e334ce71
Diego Villalba <diego.villalba01@uptc.edu.co>
2022-09-04 20:01:22 -0500

    edita front

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { getData } from "../helpers/getData";
+import { AiFillDelete } from "react-icons/ai";
+import { FaEdit } from "react-icons/fa";
+import { deleteData } from "../helpers/deleteData";
+import Agregar from "../components/Agregar";
 import { Button, Card, Modal } from "react-bootstrap";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
-import { postData } from "../helpers/postData";
-import { AiFillDelete } from "react-icons/ai";
-import { deleteData } from "../helpers/deleteData";
+import { updateData } from "../helpers/updateData";
 
 const add = Yup.object().shape({
   nombre: Yup.string().required("Este campo es requerido"),
   phone: Yup.string()
     .min(10, "N de celular muy corto")
-    .max(10, "celular muy Largo")
+    .max(10, "N celular muy Largo")
     .required("Este campo es obligatorio"),
   email: Yup.string()
     .email("Debe ser del tipo ana@ana.com")
@@ -19,86 +21,36 @@ const add = Yup.object().shape({
 });
 
 const Home = () => {
-  const [show, setShow] = useState(false);
   const [data, setData] = useState([]);
+  const [idUp, setIdUp] = useState();
+  const [show, setShow] = useState(false);
+  const [edit, setEdit] = useState({
+    nombre: "",
+    phone: "",
+    email: "",
+  });
 
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
   useEffect(() => {
     getData().then((res) => setData(res.data));
     //console.log(data);
   }, [data]);
 
+  const handleEditar = (id) => {
+    setShow(true);
+    setIdUp(id);
+    const editar = data.filter((per) => per.id == id);
+
+    setEdit({
+      nombre: editar[0].nombre,
+      phone: editar[0].phone,
+      email: editar[0].email,
+    });
+  };
+
   return (
     <div className="container">
-      <Button variant="success" onClick={handleShow}>
-        Agregar
-      </Button>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Agregar</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Formik
-            initialValues={{
-              nombre: "",
-              phone: "",
-              email: "",
-            }}
-            validationSchema={add}
-            onSubmit={(values, actions) => {
-              //console.log(values);
-              postData(values);
-              actions.resetForm();
-            }}
-          >
-            {({ errors, touched, values }) => (
-              <Form>
-                <Field
-                  type="text"
-                  placeholder="Nombre"
-                  name="nombre"
-                  className="input-add"
-                  value={values.nombre}
-                />
-                {errors.nombre && touched.nombre ? (
-                  <div>{errors.nombre}</div>
-                ) : null}
-                <Field
-                  type="text"
-                  placeholder="Celular"
-                  name="phone"
-                  className="input-add"
-                  value={values.phone}
-                />
-                {errors.phone && touched.phone ? (
-                  <div>{errors.phone}</div>
-                ) : null}
-                <Field
-                  type="email"
-                  placeholder="Email"
-                  name="email"
-                  className="input-add"
-                  value={values.email}
-                />
-                {errors.email && touched.email ? (
-                  <div>{errors.email}</div>
-                ) : null}
-
-                <Button
-                  variant="contained"
-                  color="success"
-                  type="submit"
-                  className="btn-add"
-                >
-                  Agregar
-                </Button>
-              </Form>
-            )}
-          </Formik>
-        </Modal.Body>
-      </Modal>
+      <Agregar />
 
       <section className="d-flex justify-content-center flex-wrap mt-4">
         {data.map(({ id, nombre, phone, email }) => (
@@ -116,14 +68,86 @@ const Home = () => {
               <Card.Text>
                 <strong>Email:</strong> {email}
               </Card.Text>
-              <section>
-                <span onClick={() => deleteData(id)}>
-                  <AiFillDelete className="icon-delete" />
-                </span>
+              <section className="d-flex justify-content-between">
+                <button
+                  className="btn btn-outline-danger "
+                  onClick={() => deleteData(id)}
+                >
+                  <AiFillDelete />
+                </button>
+                <button
+                  className="btn btn-outline-warning "
+                  onClick={() => handleEditar(id)}
+                >
+                  <FaEdit />
+                </button>
               </section>
             </Card.Body>
           </Card>
         ))}
+      </section>
+
+      <section>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Editar</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Formik
+              initialValues={edit}
+              validationSchema={add}
+              onSubmit={(values, actions) => {
+                //console.log(values);
+                updateData(idUp, values);
+                actions.resetForm();
+              }}
+            >
+              {({ errors, touched, values }) => (
+                <Form>
+                  <Field
+                    type="text"
+                    placeholder="Nombre"
+                    name="nombre"
+                    className="input-add"
+                    value={values.nombre}
+                  />
+                  {errors.nombre && touched.nombre ? (
+                    <div>{errors.nombre}</div>
+                  ) : null}
+                  <Field
+                    type="text"
+                    placeholder="Celular"
+                    name="phone"
+                    className="input-add"
+                    value={values.phone}
+                  />
+                  {errors.phone && touched.phone ? (
+                    <div>{errors.phone}</div>
+                  ) : null}
+                  <Field
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                    className="input-add"
+                    value={values.email}
+                  />
+                  {errors.email && touched.email ? (
+                    <div>{errors.email}</div>
+                  ) : null}
+
+                  <Button
+                    variant="contained"
+                    color="success"
+                    type="submit"
+                    className="btn-add"
+                  >
+                    Editar
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+          </Modal.Body>
+        </Modal>
       </section>
     </div>
   );
